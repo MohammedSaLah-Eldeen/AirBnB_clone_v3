b9dcb2cad83da59cb19d5dabb6095b18b23287be
Mohammed Salah <MohammedSalah.SuperM159@gmail.com>
2024-02-05 19:33:38 +0000

    task3

---
+++ api/v1/app.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+RESTAPI APP.
+"""
+import os
+from api.v1.views import app_views
+from flask import Flask
+from models import storage
+
+
+app = Flask(__name__)
+
+
+app.register_blueprint(app_views)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """
+    closes storage.
+    """
+    storage.close()
+
+
+if __name__ == "__main__":
+    
+    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+    port = os.getenv('HBNB_API_PORT', 5000) 
+    app.run(
+        host=host,
+        port=port,
+        threaded=True
+    )
